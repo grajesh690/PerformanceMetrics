c51742651d323aa5a67b4ef9937a22dc9974c682
rajesh <g.rajesh690@gmail.com>
2023-08-30 17:24:40 +0530

    Initial commit lighthouse playwright integration.

---
+++ tests/test.spec.ts
@@ -0,0 +1,38 @@
+import { chromium, test } from "@playwright/test";
+import { playAudit } from "playwright-lighthouse";
+import lighthouseDesktopConfig from "lighthouse/lighthouse-core/config/lr-desktop-config";
+import { inputUrl } from "../data/url.json";
+
+// test.describe("audit example", () => {
+inputUrl.forEach((data) => {
+  test(`Performances Test ${data}`, async () => {
+    const browser = await chromium.launch({
+      args: [`--remote-debugging-port=9222`],
+    });
+    const page = await browser.newPage();
+    console.log(data);
+    await page.goto(data);
+    await page.waitForLoadState("networkidle");
+    await playAudit({
+      page: page,
+      config: lighthouseDesktopConfig,
+      thresholds: {
+        performance: 50,
+        accessibility: 50,
+        "best-practices": 50,
+        seo: 50,
+      },
+      port: 9222,
+      reports: {
+        formats: {
+          // json: true,
+          html: true,
+        },
+        name: `audit_${data.replace("https://", "")}`,
+      },
+    });
+
+    await browser.close();
+  });
+});
+// });
